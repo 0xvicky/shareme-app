64c8cc5bf39af3df5104bb66ae76b8ab479adcd3
vicky <tyagivivek528@gmail.com>
2023-07-08 12:06:17 +0530

    Full stack shareme app started with sanity, React, Tailwindcss

---
+++ shareme-backend/schemas/Pin.js
@@ -0,0 +1,61 @@
+export default {
+  name: 'pin',
+  title: 'Pin',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'about',
+      title: 'About',
+      type: 'string',
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+    },
+    {
+      name: 'userId',
+      title: 'UserId',
+      type: 'string',
+    },
+    {
+      name: 'postedBy',
+      title: 'PostedBy',
+      type: 'postedBy',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'destination',
+      title: 'Destination',
+      type: 'url',
+    },
+    {
+      name: 'save',
+      title: 'Save',
+      type: 'array',
+      of:[ {
+        type: 'save',
+      }],
+    },
+    {
+      name: 'comments',
+      title: 'Comments',
+      type: 'array',
+      of:[ {
+        type: 'comment',
+      }],
+    },
+  ],
+}
---
+++ shareme-backend/schemas/index.js
@@ -0,0 +1,7 @@
+import User from "./User"//new Schema for user
+import Pin from "./Pin"
+import Comment from "./Comment"
+import PostedBy from "./PostedBy"
+import Save from "./Save"
+
+export const schemaTypes = [User,Pin,Comment,PostedBy,Save]
