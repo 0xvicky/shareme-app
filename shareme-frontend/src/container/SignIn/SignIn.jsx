bf4f4610734ad3f40cce00322499b5427fe0d54e
vicky <tyagivivek528@gmail.com>
2023-07-09 08:25:43 +0530

    Google OAuth is now working

--- shareme-frontend/src/container/SignIn/SignIn.jsx
+++ shareme-frontend/src/container/SignIn/SignIn.jsx
@@ -1,62 +1,51 @@
 import React from "react";
 import "./SignIn.css"
-import {GoogleLogin} from "react-google-login"
-import {FcGoogle} from "react-icons/fc"
+import { FcGoogle } from "react-icons/fc"
 import { useNavigate } from "react-router-dom";
-
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 
 const SignIn = () => {
 
-  const handleSignIn = ()=>{
-    console.log("Signed in ....")
+  const handleSignIn = (res) => {
+    console.log(res)
   }
 
   return (
     <>
-     <div className="parent flex flex-col justify-center items-center h-screen ">
-      <div className="relative w-full h-full">
-     <video
-     src="/Assets/intro.mp4"
-     autoPlay
-     muted
-     loop
-     type="video/mp4"
-     controls={false}
-     className=" h-full w-full object-cover">
-     </video>
-     <div className="absolute flex flex-col justify-center items-center top-0 bottom-0 left-0 right-0 bg-blackOverlay">
-     <div className="flex text-white justify-center items-center">
-     <img src="/Assets/camera.svg" className="mr-3"/><p className="font-bold ">Pinsta</p>App
-     </div>
+      <div className="parent flex flex-col justify-center items-center h-screen ">
+        <div className="relative w-full h-full">
+          <video
+            src="/Assets/intro.mp4"
+            autoPlay
+            muted
+            loop
+            type="video/mp4"
+            controls={false}
+            className=" h-full w-full object-cover">
+          </video>
+          <div className="absolute flex flex-col justify-center items-center top-0 bottom-0 left-0 right-0 bg-blackOverlay">
+            <div className="flex text-white justify-center items-center mb-3">
+              <img src="/Assets/camera.svg" className="mr-3" /><p className="font-bold ">Pinsta</p>App
+            </div>
 
-     <GoogleLogin
-     clientId={process.env.GOOGLE_REACT_OAUTH_CRED}
-     render={(renProps)=>(
-      <button className="flex justify-center items-center mt-3 bg-white p-3 " onClick={renProps.onClick} disabled={renProps.disabled} type="button">
-     <FcGoogle className="mr-3"/>Sign In With Google
-     </button>
-     )}
-     onSuccess={handleSignIn}
-     onFailure={handleSignIn}
-     cookiePolicy="single_host_origin"
-     >
-     </GoogleLogin>
-     </div>
+            <GoogleOAuthProvider
+              clientId="458847142767-re2lqg9i3jk57atap59lar4aatlkrahc.apps.googleusercontent.com"
+
+              >
+              <GoogleLogin
+                onSuccess={(credRes) => {
+                  console.log(credRes)
+                }}
+                onError={() => {
+                  console.log("Login Failed")
+                }}
+              />
+            </GoogleOAuthProvider>
+          </div>
+        </div>
       </div>
-     </div>
     </>
   )
 };
 
 export default SignIn;
-
-
-/**
-  <div className="flex text-white justify-center items-center">
-     <img src="/Assets/camera.svg" className="mr-3"/><p className="font-bold ">ShareMe</p>App
-     </div>
-     <button className="flex justify-center items-center mt-3 bg-white p-3 ">
-     <FcGoogle className="mr-3"/>SignIn With Google
-     </button>
-
- */
