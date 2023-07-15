ecf4c5aea3c89915db34226b937c790570a17769
vicky <tyagivivek528@gmail.com>
2023-07-15 07:36:46 +0530

    Mobile sidebar got initial options and style

--- shareme-frontend/src/components/Sidebar/Sidebar.jsx
+++ shareme-frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,19 +1,39 @@
 import React from "react";
 import "./Sidebar.css"
-import { NavLink,Link } from "react-router-dom";
-import {RiHomeFill} from "react-icons/ri"
-import {IoIosArrowForward} from "react-icons/io"
+import { NavLink, Link } from "react-router-dom";
+import { RiHomeFill } from "react-icons/ri"
+import { IoIosArrowForward } from "react-icons/io"
 
-const Sidebar = ({user, closeToggle}) => {
-  const handleSidebar = ()=>{
-    if(closeToggle) closeToggle(false)
+const Sidebar = ({ user, closeToggle }) => {
+
+  /******STYLES*********** */
+  const isActiveStyle = "flex gap-2 items-center border-r-2 mx-2  font-bold bg-gradient-to-r from-slate-400 border-black"
+  const isNotActiveStyle = ""
+
+  /*******LOGICS********** */
+
+  const handleSidebar = () => {
+    if (closeToggle) closeToggle(false)
   }
   return (
-   <div className="flex flex-col ml-3 ">
-    <Link to="/" onClick={handleSidebar} >
-    <img src="/Assets/camera.svg" alt="" />
-    </Link>
-   </div>
+    <div className="flex flex-col ml-3 my-7 md:hidden">
+      <Link to="/" onClick={handleSidebar} className="py-3">
+        <div className="flex items-center">
+          <img src="/Assets/camera.svg" className="mr-3" />
+          <p className="font-bold bg-gread">Pinsta</p>App
+        </div>
+      </Link>
+      <div className="flex flex-col gap-4 mt-3">
+        <NavLink to="/" className={({ isActive }) => {
+          return (
+            isActive ? isActiveStyle : isNotActiveStyle
+          )
+        }}><RiHomeFill />Home</NavLink>
+
+        <h3>Discover Categories</h3>
+
+      </div>
+    </div>
   )
 };
 
--- shareme-frontend/src/container/Home/Home.jsx
+++ shareme-frontend/src/container/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./Home.css"
 import { HiMenu } from "react-icons/hi"
-import {AiFillCloseCircle} from "react-icons/ai"
+import { AiFillCloseCircle } from "react-icons/ai"
 import { Routes, Route, useNavigate, Link } from "react-router-dom"
 import { Sidebar, UserProfile } from "../../components"
 import Pins from "../Pins/Pins";
@@ -25,14 +25,15 @@ const Home = () => {
 
   return (
     <>
-      <div className="flex md:flex-row flex-col transition-height duration-150 ease-linear p-2">
+      <div
+       className="flex md:flex-row flex-col transition-height duration-150 ease-linear p-2">
         <div className="hidden md:flex h-screen flex-initial">
-          <Sidebar user ={user && user} />
+          <Sidebar user={user && user} />
         </div>
-        <div className="md:hidden flex  items-center justify-between p-2 w-full shadow-md">
+        <div className="md:hidden flex  items-center justify-between p-2 w-full shadow-md ">
           <HiMenu fontSize={36} onClick={() => {
             setToggleSideBar(true)
-          }} className="cursor-pointer"/>
+          }} className="cursor-pointer w-fit" />
           <Link to="/">
             <div className="flex items-center">
               <img src="/Assets/camera.svg" className="mr-3" />
@@ -44,25 +45,24 @@ const Home = () => {
             <img src={user?.image} className="mr-3 rounded-full h-10 w-10 " />
           </Link>
         </div>
- {
-  toggleSideBar &&
- ( <>
-<div className="fixed w-5/6 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+        {
+          toggleSideBar &&
+          (<>
+            <div className="fixed w-5/6 md:hidden flex flex-col bg-slate-300 h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+              <div className="absolute w-full flex justify-end items-center p-2 ">
+                <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => { setToggleSideBar(false) }} />
+              </div>
+                <Sidebar user={user && user} closeToggle={setToggleSideBar} />
+            </div>
+          </>)
+        }
 
-  <div className="absolute w-full flex justify-end items-center p-2">
-<AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={()=>{setToggleSideBar(false)}}/>
-  </div>
-  <Sidebar user ={user && user} closeToggle={setToggleSideBar}/>
-</div>
-  </>)
- }
-
-<div className="pb-2 bg-red-500 flex-1 h-screen overflow-y-scroll" ref ={scrollRef}>
-<Routes>
-<Route path={`/user-profile/${user?._id}`} element = {<UserProfile/>} />
-<Route path="/*" element = {<Pins user={user && user}/>}/>
-</Routes>
-</div>
+        <div className="pb-2  flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+          <Routes>
+            <Route path={`/user-profile/${user?._id}`} element={<UserProfile />} />
+            <Route path="/*" element={<Pins user={user && user} />} />
+          </Routes>
+        </div>
       </div>
     </>
   )
