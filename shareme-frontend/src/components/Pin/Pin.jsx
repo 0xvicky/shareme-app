0831a72068a4799f98504cab18833a4d2050c92a
vicky <tyagivivek528@gmail.com>
2023-08-15 06:16:04 +0530

    Added Already Saved feature

--- shareme-frontend/src/components/Pin/Pin.jsx
+++ shareme-frontend/src/components/Pin/Pin.jsx
@@ -1,10 +1,67 @@
-import React from "react";
-import "./Pin.css"
-import { urlFor } from "../../client";
-const Pin = ({ pin: { image, postedBy, _id } }) => {
-    return <div>
-        <img src={urlFor(image).width(250).url()} alt="" />
-    </div>;
+import React, {useState} from "react";
+import "./Pin.css";
+import {useNavigate} from "react-router-dom";
+import {FiDownload} from "react-icons/fi";
+
+import {urlFor} from "../../client";
+import {fetchUser} from "../../utils/fetchUser";
+
+const Pin = ({pin: {image, postedBy, _id, save}}) => {
+  const [pinHovered, setpinHovered] = useState(false);
+
+  const navigate = useNavigate();
+  const userInfo = fetchUser();
+
+  const isAlreadySaved = !!save?.filter(item => item.postedBy._id === userInfo.googleId); //true or false
+  //save->[]
+  //1->[2,4,1]=>  save.filter()->[1].length->!!1->true
+  //3->[2,4,1]=>  save.filter()->[].length->!!0->false
+
+  return (
+    <div className='m-2'>
+      <div
+        onMouseEnter={() => {
+          setpinHovered(true);
+        }}
+        onMouseLeave={() => {
+          setpinHovered(false);
+        }}
+        onClick={() => {
+          navigate(`/pin-detail/${_id}`);
+        }}
+        className='bg-red-500 relative cursor-pointer hover:transform hover:scale-105 transition duration-300'>
+        <img
+          className=' border-2 border-red-500 rouned-md  w-full h-full'
+          src={urlFor(image).width(250).url()}
+          alt=''
+        />
+        {pinHovered && (
+          <div
+            className='absolute top-0 w-full h-full flex flex-col justify-between p-2 pt-1 pb-1 pl-1 pr-1 z-50'
+            style={{height: "100%"}}>
+            <div className='flex items-center justify-between'>
+              <div className='flex justify-between gap-3'>
+                <a
+                  href={`${image?.asset?.url}?dl=`}
+                  download
+                  onClick={e => {
+                    e.stopPropagation();
+                  }}
+                  className='bg-white w-10 h-10 rounded-full text-black items-center text-xl justify-center'>
+                  <FiDownload />
+                </a>
+                {isAlreadySaved ? (
+                  <button className='text-black'>Saved</button>
+                ) : (
+                  <button>Save</button>
+                )}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Pin;
--- shareme-frontend/src/container/Home/Home.jsx
+++ shareme-frontend/src/container/Home/Home.jsx
@@ -1,71 +1,97 @@
-import React, { useState, useEffect, useRef } from "react";
-import "./Home.css"
-import { HiMenu } from "react-icons/hi"
-import { AiFillCloseCircle } from "react-icons/ai"
-import { Routes, Route, useNavigate, Link } from "react-router-dom"
-import { Sidebar, UserProfile } from "../../components"
+import React, {useState, useEffect, useRef} from "react";
+import "./Home.css";
+import {HiMenu} from "react-icons/hi";
+import {AiFillCloseCircle} from "react-icons/ai";
+import {Routes, Route, useNavigate, Link} from "react-router-dom";
+import {Sidebar, UserProfile} from "../../components";
 import Pins from "../Pins/Pins";
-import { userQuery } from "../../utils/data";
-import { client } from "../../client";
+import {userQuery} from "../../utils/data";
+import {client} from "../../client";
+import {fetchUser} from "../../utils/fetchUser";
 
 const Home = () => {
   const [toggleSideBar, setToggleSideBar] = useState(false);
-  const [user, setUser] = useState()
-  const scrollRef = useRef(null)
+  const [user, setUser] = useState();
+  const scrollRef = useRef(null);
 
-  const userInfo = localStorage.getItem('user') !== "undefined" ? JSON.parse(localStorage.getItem('user')) : localStorage.clear()
+  const userInfo = fetchUser();
 
   useEffect(() => {
     const query = userQuery(userInfo?.sub);
     client.fetch(query).then(res => {
-      console.log(res)
-      setUser(res[0])//gets the user data stored in sanity datastore
-    })
+      console.log(res);
+      setUser(res[0]); //gets the user data stored in sanity datastore
+    });
   }, []);
 
   return (
     <>
-      <div
-        className="flex md:flex-row flex-col transition-height duration-150 ease-linear p-2">
-        <div className="hidden md:flex h-screen flex-initial">
+      <div className='flex md:flex-row flex-col transition-height duration-150 ease-linear p-2'>
+        <div className='hidden md:flex h-screen flex-initial'>
           <Sidebar user={user && user} />
         </div>
-        <div className="md:hidden flex  items-center justify-between p-2 w-full shadow-md ">
-          <HiMenu fontSize={36} onClick={() => {
-            setToggleSideBar(true)
-          }} className="cursor-pointer w-fit" />
-          <Link to="/">
-            <div className="flex items-center">
-              <img src="/Assets/camera.svg" className="mr-3" />
-              <p className="font-bold ">Pinsta</p>App
+        <div className='md:hidden flex  items-center justify-between p-2 w-full shadow-md '>
+          <HiMenu
+            fontSize={36}
+            onClick={() => {
+              setToggleSideBar(true);
+            }}
+            className='cursor-pointer w-fit'
+          />
+          <Link to='/'>
+            <div className='flex items-center'>
+              <img
+                src='/Assets/camera.svg'
+                className='mr-3'
+              />
+              <p className='font-bold '>Pinsta</p>App
             </div>
           </Link>
 
           <Link to={`user-profile/${user?._id}`}>
-            <img src={user?.image} className="mr-3 rounded-full h-10 w-10 shadow-md shadow-slate-600 hover:shadow-slate-800" />
+            <img
+              src={user?.image}
+              className='mr-3 rounded-full h-10 w-10 shadow-md shadow-slate-600 hover:shadow-slate-800'
+            />
           </Link>
         </div>
-        {
-          toggleSideBar &&
-          (<>
-            <div className="fixed w-5/6 md:hidden flex flex-col bg-slate-300 h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
-              <div className="absolute w-full flex justify-end items-center p-2 ">
-                <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => { setToggleSideBar(false) }} />
+        {toggleSideBar && (
+          <>
+            <div className='fixed w-5/6 md:hidden flex flex-col bg-slate-300 h-screen overflow-y-auto shadow-md z-10 animate-slide-in'>
+              <div className='absolute w-full flex justify-end items-center p-2 '>
+                <AiFillCloseCircle
+                  fontSize={30}
+                  className='cursor-pointer'
+                  onClick={() => {
+                    setToggleSideBar(false);
+                  }}
+                />
               </div>
-              <Sidebar user={user && user} closeToggle={setToggleSideBar} />
+              <Sidebar
+                user={user && user}
+                closeToggle={setToggleSideBar}
+              />
             </div>
-          </>)
-        }
+          </>
+        )}
 
-        <div className="pb-2  flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+        <div
+          className='pb-2  flex-1 h-screen overflow-y-scroll'
+          ref={scrollRef}>
           <Routes>
-            <Route path={`/user-profile/${user?._id}`} element={<UserProfile />} />
-            <Route path="/*" element={<Pins user={user && user} />} />
+            <Route
+              path={`/user-profile/${user?._id}`}
+              element={<UserProfile />}
+            />
+            <Route
+              path='/*'
+              element={<Pins user={user && user} />}
+            />
           </Routes>
         </div>
       </div>
     </>
-  )
+  );
 };
 
 export default Home;
