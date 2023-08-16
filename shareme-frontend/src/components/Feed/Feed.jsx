efaac32018b9cb74ff965d3631ebe86521b202bc
vicky <tyagivivek528@gmail.com>
2023-08-16 06:50:54 +0530

    saved functionality almost done

--- shareme-frontend/src/components/Feed/Feed.jsx
+++ shareme-frontend/src/components/Feed/Feed.jsx
@@ -1,52 +1,45 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom"
-import { client } from "../../client"
-import "./Feed.css"
+import React, {useState, useEffect} from "react";
+import {useParams} from "react-router-dom";
+import {client} from "../../client";
+import "./Feed.css";
 import Spinner from "../Spinner/Spinner";
-import { searchQuery, feedQuery } from "../../utils/data";
+import {searchQuery, feedQuery} from "../../utils/data";
 import MasonryLayout from "../MasonryLayout/MasonryLayout";
 
-
 const Feed = () => {
-    const [loading, setLoading] = useState(false);
-    const [pins, setPins] = useState(null)
-    const { categoryId } = useParams()
+  const [loading, setLoading] = useState(false);
+  const [pins, setPins] = useState(null);
+  const {categoryId} = useParams();
 
-    useEffect(() => {
-        setLoading(true)
+  useEffect(() => {
+    setLoading(true);
 
-        if (categoryId) { //will show only the feeds whose category id provided
-            const query = searchQuery(categoryId)
-            try {
-                client.fetch(query).then(res => setPins(res))
-                // setTimeout(() => setLoading(false), 1000)//to avoid flickering effect
-                setLoading(false)
-            } catch (error) {
-                console.log(`Error occured:${error}`)
-            }
-        }
-        else {
-            try {
-                client.fetch(feedQuery).then(res => {
-                    setPins(res)
-                    console.log(res)
-                })
-                setTimeout(() => {
-                    setLoading(false)
-                }, 1000);
-            } catch (error) {
-                console.log(`Error occured while fetching all pins:${error}`)
-            }
-        }//will show all feeds irrespective of category
-    }, [categoryId]);
+    if (categoryId) {
+      //will show only the feeds whose category id provided
+      const query = searchQuery(categoryId);
+      try {
+        client.fetch(query).then(res => setPins(res));
+        setTimeout(() => setLoading(false), 1000); //to avoid flickering effect
+        setLoading(false);
+      } catch (error) {
+        console.log(`Error occured:${error}`);
+      }
+    } else {
+      try {
+        client.fetch(feedQuery).then(res => {
+          setPins(res);
+        });
+        setTimeout(() => {
+          setLoading(false);
+        }, 1000);
+      } catch (error) {
+        console.log(`Error occured while fetching all pins:${error}`);
+      }
+    } //will show all feeds irrespective of category
+  }, [categoryId]);
 
-
-    if (loading) return <Spinner msg="We are adding ideas to your feed !" />
-    return (
-        <div>
-            {pins && <MasonryLayout pins={pins} />}
-        </div>
-    );
+  if (loading) return <Spinner msg='We are adding ideas to your feed !' />;
+  return <div>{pins && <MasonryLayout pins={pins} />}</div>;
 };
 
 export default Feed;
--- shareme-frontend/src/components/Pin/Pin.jsx
+++ shareme-frontend/src/components/Pin/Pin.jsx
@@ -1,21 +1,48 @@
 import React, {useState} from "react";
 import "./Pin.css";
 import {useNavigate} from "react-router-dom";
-import {FiDownload} from "react-icons/fi";
+import {MdDownloadForOffline} from "react-icons/md";
 
 import {urlFor} from "../../client";
 import {fetchUser} from "../../utils/fetchUser";
-
+import {client} from "../../client";
+import {v4 as uuidv4} from "uuid";
 const Pin = ({pin: {image, postedBy, _id, save}}) => {
   const [pinHovered, setpinHovered] = useState(false);
 
   const navigate = useNavigate();
   const userInfo = fetchUser();
 
-  const isAlreadySaved = !!save?.filter(item => item.postedBy._id === userInfo.googleId); //true or false
+  const isAlreadySaved = !!save?.filter(item => item?.postedBy?._id === userInfo.googleId)
+    ?.length; //true or false
   //save->[]
   //1->[2,4,1]=>  save.filter()->[1].length->!!1->true
   //3->[2,4,1]=>  save.filter()->[].length->!!0->false
+
+  const handleSave = id => {
+    console.log(id);
+    console.log(userInfo.sub);
+    console.log(`This is userINfo:${userInfo}`);
+    // if (!isAlreadySaved) {
+    //   client
+    //     .patch(id)
+    //     .setIfMissing({save: []})
+    //     .insert("after", "save[-1]", [
+    //       {
+    //         _key: uuidv4(),
+    //         userId: userInfo?.googleId,
+    //         postedBy: {
+    //           _type: "postedBy",
+    //           _ref: userInfo?.googleId
+    //         }
+    //       }
+    //     ])
+    //     .commit()
+    //     .then(res => {
+    //       window.location.reload();
+    //     });
+    // }
+  };
 
   return (
     <div className='m-2'>
@@ -29,33 +56,46 @@ const Pin = ({pin: {image, postedBy, _id, save}}) => {
         onClick={() => {
           navigate(`/pin-detail/${_id}`);
         }}
-        className='bg-red-500 relative cursor-pointer hover:transform hover:scale-105 transition duration-300'>
+        className='relative cursor-pointer hover:transform hover:scale-105 transition duration-300'>
         <img
-          className=' border-2 border-red-500 rouned-md  w-full h-full'
+          className=' rouned-md  w-full h-full'
           src={urlFor(image).width(250).url()}
           alt=''
         />
         {pinHovered && (
           <div
-            className='absolute top-0 w-full h-full flex flex-col justify-between p-2 pt-1 pb-1 pl-1 pr-1 z-50'
+            className='absolute top-0 w-full h-full flex flex-col justify-between p-2 z-50'
             style={{height: "100%"}}>
             <div className='flex items-center justify-between'>
-              <div className='flex justify-between gap-3'>
+              <div className='flex gap-2 '>
                 <a
                   href={`${image?.asset?.url}?dl=`}
                   download
                   onClick={e => {
                     e.stopPropagation();
                   }}
-                  className='bg-white w-10 h-10 rounded-full text-black items-center text-xl justify-center'>
-                  <FiDownload />
+                  className='bg-white w-8 h-8 flex rounded-full text-black items-center text-2xl justify-center opacity-75 hover:opacity-100 hover:shadow-md outline-none'>
+                  <MdDownloadForOffline />
                 </a>
-                {isAlreadySaved ? (
-                  <button className='text-black'>Saved</button>
-                ) : (
-                  <button>Save</button>
-                )}
               </div>
+              {isAlreadySaved ? (
+                <button
+                  className='bg-red-500 text-white p-2 rounded-lg opacity-75 hover:opacity-100 outline-none hover:shadow-md'
+                  onClick={e => {
+                    handleSave(_id);
+                  }}>
+                  {save?.length} Saved
+                </button>
+              ) : (
+                <button
+                  className='bg-red-500 text-white p-2 rounded-lg opacity-75 hover:opacity-100 outline-none hover:shadow-md'
+                  onClick={e => {
+                    e.stopPropagation();
+                    handleSave(_id);
+                  }}>
+                  Save
+                </button>
+              )}
             </div>
           </div>
         )}
